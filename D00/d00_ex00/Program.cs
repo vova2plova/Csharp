3f4fc61c9fe976aae7f6a6c01e5c01628c966f49
v.yakovlev <wwevovancik@gmail.com>
2022-02-08 21:43:32 +0300

    Расстояние Левешнтейна

---
+++ D00/d00_ex00/Program.cs
@@ -0,0 +1,89 @@
+﻿using static System.Math;
+
+double sum;
+double rate;
+int term;
+
+/*Считываем данные с консоли, завершаем работу приложения с ошибкой, если что-то пошло не так*/
+
+try
+{
+    Console.WriteLine("Введите сумма кредита");
+    sum = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите процентную ставку");
+    rate = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите кол-во месяцев кредита");
+    term = Convert.ToInt32(Console.ReadLine());
+    if (term < 0 || rate < 0 || sum < 0)
+        throw new Exception();
+}
+catch
+{ 
+    Console.WriteLine("Something went wrong.Check your input and retry.");
+    return;
+}
+CreditCalculator(sum, rate, term);
+
+/*Высчитываем общий ежемесячный платёж*/
+
+double CalcSumMonthPayment(double newSum, double newI, int NewTerm)
+{
+    return ((newSum * newI * Pow(1 + newI, NewTerm))
+        /(Pow(1+newI,NewTerm) - 1));
+}
+
+/*Высчитываем процентную ставку по займу в месяц*/
+
+double CalcI(double newRate)
+{
+    return (newRate / 12 / 100);
+}
+
+/*Высчитываем проценты ежемесячного платежа*/
+
+double CalcPercent(double newSum, double newRate, int newDaysOfMonth, int newDaysOfYear)
+{
+    return ((newSum * newRate * (newDaysOfMonth)) / (100 * newDaysOfYear));
+}
+
+/*Если при округлении до 2х знаков после запятой остаётся один знак добавляем нолик*/
+
+string PrintNumber(double newNumber)
+{
+    string[] str = Round(newNumber,2).ToString().Split(',');
+    if (str[1].Length < 2)
+        return (Round(newNumber,2).ToString() + "0");
+    else
+        return (Round(newNumber,2).ToString());
+}
+
+/*Кредитный калькулятор*/
+
+void CreditCalculator(double newSum, double newRate, int NewTerm)
+{
+    double _sum = newSum;
+    double _rate = newRate;
+    int _term = term;
+    DateTime date = new DateTime(2021, 5, 1);
+    int j = 0;
+    double _i = CalcI(newRate);
+    double _monthPayment = CalcSumMonthPayment(_sum, _i, _term);
+    while (j < _term)
+    {
+        int _dayInMonth = DateTime.DaysInMonth(date.Year, date.Month);
+        int _dayInYear = DateTime.IsLeapYear(date.Year) ? 366 : 365;
+        date = date.AddMonths(1);
+        double _percent = CalcPercent(_sum, _rate, _dayInMonth, _dayInYear);
+
+        if (j < _term - 1)
+            Console.WriteLine($"{j + 1}\t\t{date.Month}/{date.Day}/{date.Year}\t\t" +
+                $"{PrintNumber(_monthPayment)}\t\t{PrintNumber(_monthPayment - _percent)}\t\t" +
+                $"{PrintNumber(_percent)}\t\t{PrintNumber(_sum - (_monthPayment - _percent))}");
+        else
+            Console.WriteLine($"{j + 1}\t\t{date.Month}/{date.Day}/{date.Year}\t\t" +
+                $"{PrintNumber(_sum + _percent)}\t\t{PrintNumber(_monthPayment)}\t\t" +
+                $"{PrintNumber(_percent)}\t\t{0}");
+        j++;
+        _sum = _sum - (_monthPayment - _percent);
+    }
+}
---
+++ D00/d00_ex01/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.IO;
+using System;
+
+Console.WriteLine("Enter Name:");
+string? name = Console.ReadLine();
+string path = @"C:\Users\user\Desktop\trainingC#\Ex00\d00_ex01\us_names.txt"; /*Путь до файла*/
+FindRightName(name);
+
+/*Функция нахождения имени где дистанция Левенштейна <= 1*/
+
+void FindRightName(string _name)
+{
+    if (name == null)
+    {
+        Console.WriteLine("Your name was not found.");
+        return;
+    }
+    using (StreamReader sr = new StreamReader(path))
+    {
+        string s;
+        while ((s = sr.ReadLine()) != null)
+        {
+            if (LevenshteinDistance(s, _name) == 0)
+            {
+                Console.WriteLine($"Hello {s}");
+                return;
+            }
+            if (LevenshteinDistance(s, _name) < 2)
+            {
+                Console.WriteLine(LevenshteinDistance(s, _name));
+                Console.WriteLine($"Did you mean “{s}”? Y/N");
+                string? answ = Console.ReadLine();
+                if (answ == null)
+                {
+                    Console.WriteLine("Something went wrong. Check your input and retry.");
+                    return;
+                }
+                if (answ.Equals("Y"))
+                {
+                    Console.WriteLine($"Hello {s}");
+                    return;
+                }
+                else if (answ.Equals("N"))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong. Check your input and retry.");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine("Your name was not found.");
+    }
+}
+
+/*Нахождения минимума среди 3х чисел*/
+
+static int Minimum(int a, int b, int c) => (a = a < b ? a : b) < c ? a : c;
+
+/*Дистанция Левештейна*/
+
+static int LevenshteinDistance(string firstWord, string secondWord)
+{
+    var n = firstWord.Length + 1;
+    var m = secondWord.Length + 1;
+    var matrixD = new int[n, m];
+
+    const int deletionCost = 1;
+    const int insertionCost = 1;
+
+    for (var i = 0; i < n; i++)
+    {
+        matrixD[i, 0] = i;
+    }
+
+    for (var j = 0; j < m; j++)
+    {
+        matrixD[0, j] = j;
+    }
+
+    for (var i = 1; i < n; i++)
+    {
+        for (var j = 1; j < m; j++)
+        {
+            var substitutionCost = firstWord[i - 1] == secondWord[j - 1] ? 0 : 1;
+
+            matrixD[i, j] = Minimum(matrixD[i - 1, j] + deletionCost,          // удаление
+                                    matrixD[i, j - 1] + insertionCost,         // вставка
+                                    matrixD[i - 1, j - 1] + substitutionCost); // замена
+        }
+    }
+
+    return matrixD[n - 1, m - 1];
+}
